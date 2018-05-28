48cdb6c4422600a8b04e71b7d09ee98d5dbc54a2
Diego Lirio <diegolirio.dl@gmail.com>
2018-05-28 01:34:07 -0300

    ::hystrix::The Circuit Break

--- demo-user-microservice/src/main/java/io/github/diegolirio/demousermicroservice/Application.java
+++ demo-user-microservice/src/main/java/io/github/diegolirio/demousermicroservice/Application.java
@@ -1,17 +1,11 @@
 package io.github.diegolirio.demousermicroservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableHystrix
-@EnableHystrixDashboard
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
