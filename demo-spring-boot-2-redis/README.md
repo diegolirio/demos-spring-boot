4f28a7f99c4fcff8f9f40382efa98e56c933dcc6
Tatiane <tatysantos.dl@gmail.com>
2020-03-01 22:17:12 -0300

    Deleted  demo-spring-boot-2-redis

--- demo-spring-boot-2-redis/README.md
+++
@@ -1,3 +0,0 @@
-# Spring Boot Data Redis
-
-- More Details -> https://gist.github.com/diegolirio/fcd6642b48509a4d687b25aa3027030a
--- demo-spring-boot-2-redis/src/main/java/io/github/diegolirio/demospringboot2redis/customer/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.diegolirio.demospringboot2redis.customer;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, String> {
-}
