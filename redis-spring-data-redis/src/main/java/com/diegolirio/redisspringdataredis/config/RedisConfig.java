ff738407787aaaf67bc63549858d9b2fe8b58731
zuleica <diegolirio.dl@gmail.com>
2020-06-04 12:34:49 -0300

    Modo Cluster Configured

--- redis-spring-data-redis/src/main/java/com/diegolirio/redisspringdataredis/config/RedisConfig.java
+++ redis-spring-data-redis/src/main/java/com/diegolirio/redisspringdataredis/config/RedisConfig.java
@@ -3,31 +3,53 @@ package com.diegolirio.redisspringdataredis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import redis.clients.jedis.Jedis;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    String host;
+    @Value("${spring.redis.port}")
+    int port;
+
+    List<String> clusterNodes = Arrays.asList(host + ":" + port);
+
     @Bean
-    public Jedis jedis(@Value("${spring.redis.host}") final String host,
-                       @Value("${spring.redis.port}") final int port) {
+    RedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory(new RedisClusterConfiguration(clusterNodes));
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
+    }
+
+    @Bean
+    public Jedis jedis() {
         return new Jedis(host, port);
     }
 
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory(final String host, final int port) {
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration(host, port));
-    }
 
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate(@Value("${spring.redis.host}") final String host,
-                                             @Value("${spring.redis.port}") final int port) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory(host, port));
-        return template;
-    }
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory(final String host, final int port) {
+//        return new JedisConnectionFactory(new RedisStandaloneConfiguration(host, port));
+//    }
+//
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate(@Value("${spring.redis.host}") final String host,
+//                                             @Value("${spring.redis.port}") final int port) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory(host, port));
+//        return template;
+//    }
 
 }
