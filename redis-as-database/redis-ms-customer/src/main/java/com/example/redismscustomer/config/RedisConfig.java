58afdb1dee50b6e42b2d33859ad14cdecb10c103
zuleica <diegolirio.dl@gmail.com>
2020-06-08 01:00:54 -0300

    Redis as DB

--- redis-as-database/redis-ms-customer/src/main/java/com/example/redismscustomer/config/RedisConfig.java
+++ redis-as-database/redis-ms-customer/src/main/java/com/example/redismscustomer/config/RedisConfig.java
@@ -1,15 +1,20 @@
 package com.example.redismscustomer.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties.Jedis;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
     public Jedis jedis() {
-        return new Jedis(host, port);
+        return new Jedis();
     }
 
     @Bean
-    public JedisCluster jedisCluster() {
+    public JedisClusterConnection jedisCluster() {
         return null;
     }
 
--- redis-as-database/redis-ms-customer/src/main/java/com/example/redismscustomer/customer/presentation/CustomerRestController.java
+++ redis-as-database/redis-ms-customer/src/main/java/com/example/redismscustomer/customer/presentation/CustomerRestController.java
@@ -1,6 +1,10 @@
 package com.example.redismscustomer.customer.presentation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/v1/customers")
 public class CustomerRestController {
 
 
