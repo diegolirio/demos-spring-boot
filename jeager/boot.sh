dec0f74ff6c2eb49fcc17ab900175e5bdae4c591
nnmuniz <pcnathalya@hotmail.com>
2020-08-14 14:31:41 -0300

    Downgrade version java

--- jeager/boot.sh
+++ jeager/boot.sh
@@ -1,6 +1,6 @@
 cd demo-jeager-customer
-sudo mvn clean package docker:build
+mvn clean package docker:build
 cd ../demo-jeager
-sudo mvn clean package docker:build
-sudo docker-compose down
-sudo docker-compose up -d
+mvn clean package docker:build
+docker-compose down
+docker-compose up -d
