93a47bd9f4f122b0159101c6ab1fea94064f358f
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-11-08 14:44:02 -0300

    First Commit

---
+++ some-design-pattern/src/main/kotlin/com/devspoint/somedesignpattern/SomeDesignPatternApplication.kt
@@ -0,0 +1,11 @@
+package com.devspoint.somedesignpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SomeDesignPatternApplication
+
+fun main(args: Array<String>) {
+	runApplication<SomeDesignPatternApplication>(*args)
+}
