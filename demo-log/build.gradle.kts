3ce855790bb75cc4093698b642b8d6d809b4ff58
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2021-12-13 12:44:12 -0300

    Deploy

---
+++ demo-log/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.1"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.0"
+	kotlin("plugin.spring") version "1.6.0"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	jcenter()
+	maven ( url = "https://nexus.pagseguro.intranet/repository/public" )
+	maven ( url = "https://nexus.pagseguro.intranet/repository/uol.pagseguro/" )
+	maven ( url = "https://nexus.pagseguro.intranet/repository/uol-3rd/" )
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("uol.pagseguro:aspect-log-interceptor:1.0")
+
+	implementation("org.apache.logging.log4j:log4j-api:2.15.0")
+	implementation("org.apache.logging.log4j:log4j-to-slf4j:2.15.0")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ demo-log/src/main/kotlin/com/example/demolog/DemoLogApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demolog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoLogApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoLogApplication>(*args)
+}
