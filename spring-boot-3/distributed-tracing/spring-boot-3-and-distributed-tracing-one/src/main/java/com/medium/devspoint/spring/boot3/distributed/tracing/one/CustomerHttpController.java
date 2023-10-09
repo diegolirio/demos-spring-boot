2cb06d94754c198b93b22d7ba01b1bf70b600e5f
Diego Lírio <diegolirio.dl@gmail.com>
2023-10-09 04:39:23 -0700

    Customer-Details for the distributed-tracing

--- spring-boot-3/distributed-tracing/spring-boot-3-and-distributed-tracing-one/src/main/java/com/medium/devspoint/spring/boot3/distributed/tracing/one/CustomerHttpController.java
+++ spring-boot-3/distributed-tracing/spring-boot-3-and-distributed-tracing-one/src/main/java/com/medium/devspoint/spring/boot3/distributed/tracing/one/CustomerHttpController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,34 @@ import io.micrometer.observation.annotation.Observed;
 @RestController
 @RequestMapping("/customers")
 public class CustomerHttpController {
-    
-    Logger log = LoggerFactory.getLogger(CustomerHttpController.class);
 
+    Logger log = LoggerFactory.getLogger(CustomerHttpController.class);
+    private final ServiceTwo serviceTwo;
+    
+    public CustomerHttpController(ServiceTwo serviceTwo) {
+        this.serviceTwo = serviceTwo;
+    }
 
     @GetMapping
     @Observed
     public List<String> getAll() {
-        var list = List.of("Diego", "Jonh");
-        log.info("list={}", list);
+        log.info("m=getAll, step=init");
+        var list = serviceTwo.getCustomers();
+        log.info("m=getAll, step=end, list={}", list);
         return list;
+    }
+
+
+
+}
+
+@Component
+class ServiceTwo {
+
+    Logger log = LoggerFactory.getLogger(ServiceTwo.class);
+
+    public List<String> getCustomers() {
+        return List.of("Diego", "Jonh");
     }
 
 }
