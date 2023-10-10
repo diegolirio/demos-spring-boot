47aaf9be372df5fed3705c1049433f704f41fbb4
Diego Lírio <diegolirio.dl@gmail.com>
2023-10-09 22:15:46 -0300

    +

--- spring-boot-3/distributed-tracing/spring-boot-3-and-distributed-tracing-one/src/main/java/com/medium/devspoint/spring/boot3/distributed/tracing/one/Customer.java
+++
@@ -1,4 +0,0 @@
-package com.medium.devspoint.spring.boot3.distributed.tracing.one;
-
-public class Customer {
-}
--- spring-boot-3/distributed-tracing/spring-boot-3-and-distributed-tracing-one/src/main/java/com/medium/devspoint/spring/boot3/distributed/tracing/one/Detail.java
+++
@@ -1,2 +0,0 @@
-package com.medium.devspoint.spring.boot3.distributed.tracing.one;public record Detail() {
-}
